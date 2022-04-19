5aee33e52c76279506827a5d39c15a23bfa12604
zayanahmed925 <rjraju925@gmail.com>
2022-04-19 16:04:52 +0600

    Page title set

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import PageTitle from '../../Shared/PageTitle/PageTitle';
 
 const About = () => {
     return (
         <div>
+            <PageTitle title='About'></PageTitle>
             <h1>This is about</h1>
         </div>
     );
--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -7,6 +7,7 @@ import auth from '../../../firebase.init';
 import SocialLogin from '../SocialLogin/SocialLogin';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import PageTitle from '../../../Shared/PageTitle/PageTitle';
 
 const Login = () => {
     const emailRef = useRef('');
@@ -56,6 +57,7 @@ const Login = () => {
     }
     return (
         <div className='container w-50 mx-auto'>
+            <PageTitle title='Login'></PageTitle>
             <h2 className='text-primary text-center mt-3'>Please login</h2>
             <Form onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
